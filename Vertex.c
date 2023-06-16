18f387393af6196ce0b01a49f5198224cfcd9456
Ilya <ividivi67@gmail.com>
2023-06-16 17:31:30 +0300

    feat: add Vertex.c

---
+++ Vertex.c
@@ -0,0 +1,9 @@
+#include "Vertex.h"
+
+struct Vertex* createVertex(char* computerName, unsigned int port) {
+	struct Vertex* v = (struct Vertex* )malloc(sizeof(struct Vertex));
+	v->computerName = computerName;
+	v->port = port;
+
+	return v;
+}
