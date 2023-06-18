4c29939368be7eb7dece3406b6261565fcd5fcef
Ilya <ividivi67@gmail.com>
2023-06-18 15:33:28 +0300

    feat: add portsAmount to createEdge()

--- Edge.c
+++ Edge.c
@@ -1,10 +1,11 @@
 #include "Edge.h"
 #include <malloc.h>
 
-struct Edge* createEdge(unsigned int* ports, unsigned int toVertexNumber) {
+struct Edge* createEdge(unsigned int portsAmount, unsigned int* ports, unsigned int toVertexNumber) {
 	struct Edge* e = (struct Edge* )malloc(sizeof(struct Edge));
     if (e == NULL) return NULL;
 
+    e->portsAmount = portsAmount;
 	e->ports = ports;
 	e->toVertexNumber = toVertexNumber;
 
--- Edge.h
+++ Edge.h
@@ -2,11 +2,12 @@
 #define EDGE_H
 
 struct Edge {
+    unsigned int portsAmount;
 	unsigned int* ports;
 	unsigned int toVertexNumber;
 };
 
-struct Edge* createEdge(unsigned int* ports, unsigned int toVertexNumber);
+struct Edge* createEdge(unsigned int portsAmount, unsigned int* ports, unsigned int toVertexNumber);
 void deleteEdge(struct Edge* edge);
 
 #endif
