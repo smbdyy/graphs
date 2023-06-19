d59a73d9481dfc31d6e5bb4f011ba4542c5bc831
Ilya <ividivi67@gmail.com>
2023-06-19 19:28:49 +0300

    fix: enqueue return value

--- Queue.c
+++ Queue.c
@@ -48,6 +48,8 @@ bool enqueue(struct Queue* queue, unsigned int value) {
         queue->back->next = node;
         queue->back = node;
     }
+
+    return true;
 }
 
 unsigned int dequeue(struct Queue* queue) {
